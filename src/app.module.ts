ef71de44e5a3246e42fd6232ef6addc4967d164b
ppwarin9 <knotgod3@gmail.com>
2026-07-21 18:02:22 +0700

    feat: add prisma exception filter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,25 @@
-import { Module } from '@nestjs/common';
+import { ExceptionFilter, Module, Type } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { validateEnv } from '@/config/env.validation';
+import { APP_FILTER } from '@nestjs/core';
+import { PrismaExceptionFilter } from '@/common/filters/prisma-exception.filter';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-      validate: validateEnv,
+      validate: validateEnv as (
+        config: Record<string, any>,
+      ) => Record<string, any>,
     }),
     DatabaseModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: PrismaExceptionFilter as Type<ExceptionFilter>,
+    },
+  ],
 })
 export class AppModule {}
---
+++ src/common/filters/prisma-exception.filter.ts
@@ -0,0 +1,57 @@
+import { Prisma } from '@/database/generated/prisma/client';
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpStatus,
+} from '@nestjs/common';
+import { Response } from 'express';
+
+interface IPrismaError {
+  code: string;
+  message: string;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-argument
+@Catch(Prisma.PrismaClientKnownRequestError)
+export class PrismaExceptionFilter implements ExceptionFilter {
+  catch(exception: unknown, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+
+    let status = HttpStatus.INTERNAL_SERVER_ERROR;
+    let message = 'Internal server error';
+    let errorCode = 'Unknown';
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    if (exception instanceof Prisma.PrismaClientKnownRequestError) {
+      const prismaError = exception as IPrismaError;
+      errorCode = prismaError.code;
+
+      switch (prismaError.code) {
+        case 'P2002':
+          status = HttpStatus.CONFLICT;
+          message = 'Conflict: Data already exists or unique constraint failed';
+          break;
+        case 'P2025':
+          status = HttpStatus.NOT_FOUND;
+          message = 'Not Found: Record to update/delete does not exist';
+          break;
+        case 'P2003':
+          status = HttpStatus.BAD_REQUEST;
+          message = 'Bad Request: Foreign key constraint failed';
+          break;
+        default:
+          status = HttpStatus.BAD_REQUEST;
+          message = prismaError.message.replace(/\n/g, '');
+          break;
+      }
+    }
+
+    response.status(status).json({
+      statusCode: status,
+      message: message,
+      error: errorCode,
+    });
+  }
+}
