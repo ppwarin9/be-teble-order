54d386818a2c91451f9fa16d9c5856de923820e9
ppwarin9 <knotgod3@gmail.com>
2026-07-21 16:46:53 +0700

    feat: add prisma service

---
+++ src/database/prisma.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaClient } from './generated/prisma/client';
+import { ConfigService } from '@nestjs/config';
+import { EnvVariable } from '../config/env.validation';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+@Injectable()
+export class PrismaService extends PrismaClient {
+  constructor(
+    private readonly configService: ConfigService<EnvVariable, true>,
+  ) {
+    const adapter = new PrismaPg({
+      connectionString: configService.get('DATABASE_URL', { infer: true }),
+    });
+    super({ adapter });
+  }
+}
