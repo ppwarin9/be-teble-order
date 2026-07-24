b487b4292f33f0fa4babf3957409946e12a33c3e
ppwarin9 <knotgod3@gmail.com>
2026-07-24 14:54:31 +0700

    feat: add store-setting repository

---
+++ src/store-setting/store-setting.repository.ts
@@ -0,0 +1,28 @@
+import { Prisma, StoreSetting } from '@/database/generated/prisma/client';
+import { PrismaService } from '@/database/prisma.service';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class StoreSettingRepository {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findSingleton(): Promise<StoreSetting | null> {
+    return this.prisma.storeSetting.findFirst();
+  }
+
+  async create(data: Prisma.StoreSettingCreateInput): Promise<StoreSetting> {
+    return this.prisma.storeSetting.create({
+      data,
+    });
+  }
+
+  async update(
+    id: string,
+    data: Prisma.StoreSettingUpdateInput,
+  ): Promise<StoreSetting> {
+    return this.prisma.storeSetting.update({
+      where: { id },
+      data,
+    });
+  }
+}
